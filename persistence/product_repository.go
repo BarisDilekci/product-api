c21cbcffed1bd63b0e96c56e23e15b543946f491
BarisDilekci <barixmenuygulama@gmail.com>
2025-04-06 01:06:58 +0300

    fix: fixed duplicate code structure in product_repository.go

--- persistence/product_repository.go
+++ persistence/product_repository.go
@@ -2,6 +2,7 @@ package persistence
 
 import (
 	"context"
+	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/labstack/gommon/log"
 	"product-app/domain"
@@ -30,19 +31,8 @@ func (productRepository *ProductRepository) GettAllProducts() []domain.Product {
 		log.Error("Error while getting all products %v", err)
 		return []domain.Product{}
 	}
-	var products = []domain.Product{}
-	var id int64
-	var name string
-	var price float32
-	var discount float32
-	var store string
 
-	for productRows.Next() {
-		productRows.Scan(&id, &name, &price, &discount, &store)
-		products = append(products, domain.Product{id, name, price, discount, store})
-	}
-
-	return products
+	return extractProductFromRows(productRows)
 }
 
 func (productRepository *ProductRepository) GetAllProductsByStore(storeName string) []domain.Product {
@@ -55,6 +45,11 @@ func (productRepository *ProductRepository) GetAllProductsByStore(storeName stri
 		log.Error("Error while getting all products %v", err)
 		return []domain.Product{}
 	}
+
+	return extractProductFromRows(productRows)
+}
+
+func extractProductFromRows(productRows pgx.Rows) []domain.Product {
 	var products = []domain.Product{}
 	var id int64
 	var name string
@@ -68,4 +63,5 @@ func (productRepository *ProductRepository) GetAllProductsByStore(storeName stri
 	}
 
 	return products
+
 }
