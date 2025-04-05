2c0697fa0cf4c4bc7a76c8e415d6b97016d860ca
BarisDilekci <barixmenuygulama@gmail.com>
2025-04-05 18:12:43 +0300

    Fix: Add missing module dependencies for pgxpool

--- domain/Product.go
+++ domain/Product.go
@@ -3,7 +3,7 @@ package domain
 type Product struct {
 	Id       int64
 	Name     string
-	Product  float32
+	Price    float32
 	Discount float32
 	Store    string
 }
--- test/infrastructure/product_repository_test.go
+++ test/infrastructure/product_repository_test.go
@@ -4,11 +4,13 @@ import (
 	"context"
 	"fmt"
 	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/stretchr/testify/assert"
 	"os"
 	"product-app/common/postgresql"
+	"product-app/domain"
+	"product-app/persistence"
 	"testing"
 )
-import "product-app/persistence"
 
 var productRepository persistence.IProductRepository
 var dbPool *pgxpool.Pool
@@ -21,12 +23,11 @@ func TestMain(m *testing.M) {
 		Host:                  "localhost",
 		Port:                  "6432",
 		DbName:                "productapp",
-		UserName:              "postgress",
-		Password:              "yourpassword",
+		UserName:              "postgres",
+		Password:              "postgres",
 		MaxConnections:        "10",
 		MaxConnectionIdleTime: "30s",
 	})
-
 	productRepository = persistence.NewProductRepository(dbPool)
 	fmt.Println("Before all tests")
 	exitCode := m.Run()
@@ -34,8 +35,51 @@ func TestMain(m *testing.M) {
 	os.Exit(exitCode)
 }
 
+func setup(ctx context.Context, dbPool *pgxpool.Pool) {
+	TestDataInitialize(ctx, dbPool)
+}
+func clear(ctx context.Context, dbPool *pgxpool.Pool) {
+	TruncateTestData(ctx, dbPool)
+}
+
 func TestGetAllProducts(t *testing.T) {
-	fmt.Println("TestGetAllProducts")
-	fmt.Println(productRepository)
-	fmt.Println(dbPool)
+	setup(ctx, dbPool)
+
+	expectedProducts := []domain.Product{
+		{
+			Id:       1,
+			Name:     "AirFryer",
+			Price:    3000.0,
+			Discount: 22.0,
+			Store:    "ABC TECH",
+		},
+		{
+			Id:       2,
+			Name:     "Ütü",
+			Price:    1500.0,
+			Discount: 10.0,
+			Store:    "ABC TECH",
+		},
+		{
+			Id:       3,
+			Name:     "Çamaşır Makinesi",
+			Price:    10000.0,
+			Discount: 15.0,
+			Store:    "ABC TECH",
+		},
+		{
+			Id:       4,
+			Name:     "Lambader",
+			Price:    2000.0,
+			Discount: 0.0,
+			Store:    "Dekorasyon Sarayı",
+		},
+	}
+	t.Run("GetAllProducts", func(t *testing.T) {
+		actualProducts := productRepository.GettAllProducts()
+		assert.Equal(t, 4, len(actualProducts))
+		assert.Equal(t, expectedProducts, actualProducts)
+	})
+
+	clear(ctx, dbPool)
 }
