acc124bb9e76a69b4e81002c0adc88a98f86b8c8
BarisDilekci <barixmenuygulama@gmail.com>
2025-04-10 02:47:01 +0300

    feat: controller layer was written for AddProduct and GetAllProducts

--- controller/product_controller.go
+++ controller/product_controller.go
@@ -2,6 +2,9 @@ package controller
 
 import (
 	"github.com/labstack/echo/v4"
+	"net/http"
+	"product-app/controller/request"
+	"product-app/controller/response"
 	"product-app/service"
 )
 
@@ -26,11 +29,32 @@ func (productController *ProductController) GetProductById(c echo.Context) error
 }
 
 func (productController *ProductController) GetAllProducts(c echo.Context) error {
-	return nil
+	store := c.QueryParam("store")
+
+	if len(store) == 0 {
+		allProducts := productController.productService.GetAllProducts()
+		return c.JSON(http.StatusOK, response.ToResponseList(allProducts))
+	}
+	productsWithGivenStore := productController.productService.GetAllProductsByStore(store)
+	return c.JSON(http.StatusOK, response.ToResponseList(productsWithGivenStore))
 }
 
 func (productController *ProductController) AddProduct(c echo.Context) error {
-	return nil
+	var addProductRequest request.AddProductRequest
+	bindErr := c.Bind(&addProductRequest)
+	if bindErr != nil {
+		return c.JSON(http.StatusBadRequest, response.ErrorResponse{
+			ErrorDescription: bindErr.Error(),
+		})
+	}
+	err := productController.productService.Add(addProductRequest.ToModel())
+
+	if err != nil {
+		return c.JSON(http.StatusUnprocessableEntity, response.ErrorResponse{
+			ErrorDescription: err.Error(),
+		})
+	}
+	return c.NoContent(http.StatusCreated)
 }
 
 func (productController *ProductController) UpdatePrice(c echo.Context) error {
